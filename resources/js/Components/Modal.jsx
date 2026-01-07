0d91ae39fcd5eb9fb12dc459c31ab764beca8be8
Indra Kusumah <indrakusumah1@gmail.com>
2026-01-07 18:16:03 +0700

    Fix Modal for Tailwind v4 opacity syntax

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -40,7 +40,7 @@ export default function Modal({
             <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
                 {/* Backdrop */}
                 <div
-                    className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75"
+                    className="fixed inset-0 transition-opacity bg-gray-500/75"
                     aria-hidden="true"
                     onClick={onClose}
                 ></div>
